18b4d2dd1e9bfe76ebd71f5553961bf9afa859ee
Fabiano Morais <fmorais@gmail.com>
2016-08-11 08:52:33 +1000

    adding todo next

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -8,5 +8,15 @@ class FeedbacksController < ApplicationController
   end
 
 
+def create
+  @facilitation = Facilitation.find(params[:facilitation_id])
+  @feedback = @facilitation.feedbacks.build(params[:feedback])
+  if @feedback.save
+    # faz algo
+  else
+    #f az outra coisa.
+  end
+end
+
 
 end
